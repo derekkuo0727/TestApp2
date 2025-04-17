963d4b4d339a0465f68e17709973ad8970d85ab4
DerekKuo <derek.b92902121@gmail.com>
2025-04-17 20:17:32 +0800

    Initial Commit

---
+++ TestApp2/TestApp2App.swift
@@ -0,0 +1,32 @@
+//
+//  TestApp2App.swift
+//  TestApp2
+//
+//  Created by Derek Kuo on 4/17/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TestApp2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
